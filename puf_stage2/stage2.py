53124db63268c2a62e8d7352946da3c83d878027
Jacob C <44846403+chusloj@users.noreply.github.com>
2021-01-27 10:55:53 -0500

    minimal updates for stage_2+3

--- puf_stage2/stage2.py
+++ puf_stage2/stage2.py
@@ -2,7 +2,7 @@ import os
 import glob
 import numpy as np
 import pandas as pd
-from dataprep import dataprep
+from dataprep_INT import dataprep
 
 
 CUR_PATH = os.path.abspath(os.path.dirname(__file__))
